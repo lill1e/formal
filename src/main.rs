262992b2b460adfac96b7f4429fd69276fa10fdb
lillie <onlinechronically@protonmail.com>
2025-10-31 10:59:21 -0400

    add: testing code in main function

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
+mod interp;
+mod lexer;
+mod parser;
+
+use lexer::lex;
+use parser::parse;
+
 fn main() {
+    let l = lex(String::from("21+10+5+3+2+1"));
+    let p = parse(l);
+    println!("expected result: {}", p.interpret());
+    println!("parse: {}", &p.to_string());
 }
