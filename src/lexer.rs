3d83eacd5ed7392edaf58ddf3619b7a7a3913339
lillie <onlinechronically@protonmail.com>
2025-12-29 19:12:02 -0500

    update: lexer to support conditionals+operators

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,15 +1,29 @@
 use std::{iter::Peekable, str::Chars};
 
+use anyhow::{Result, bail};
+
 #[derive(Debug, Clone)]
 pub enum SymbolToken {
     Plus,
     Minus,
+    Bang,
     Equals,
+    DoubleEquals,
+    NotEquals,
+    LeftBrace,
+    RightBrace,
+    Void,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 }
 
 #[derive(Debug, Clone)]
 pub enum Keyword {
     Let,
+    If,
+    Else,
 }
 
 #[derive(Debug, Clone)]
@@ -19,6 +33,7 @@ pub enum Token {
     Semicolon,
     Keyword(Keyword),
     Identifier(String),
+    Boolean(bool),
 }
 
 fn lex_number(iter: &mut Peekable<Chars<'_>>) -> Token {
@@ -48,6 +63,11 @@ pub fn lex_symbol(c: &char) -> Option<SymbolToken> {
         '+' => Some(SymbolToken::Plus),
         '-' => Some(SymbolToken::Minus),
         '=' => Some(SymbolToken::Equals),
+        '{' => Some(SymbolToken::LeftBrace),
+        '}' => Some(SymbolToken::RightBrace),
+        '!' => Some(SymbolToken::Bang),
+        '>' => Some(SymbolToken::Greater),
+        '<' => Some(SymbolToken::Less),
         _ => None,
     }
 }
@@ -59,40 +79,50 @@ pub fn lex_word(iter: &mut Peekable<Chars<'_>>) -> Token {
     }
     match acc.as_str() {
         "let" => Token::Keyword(Keyword::Let),
+        "true" => Token::Boolean(true),
+        "false" => Token::Boolean(false),
+        "void" => Token::Symbol(SymbolToken::Void),
         _ => Token::Identifier(acc),
     }
 }
 
-pub fn lex(s: String) -> Vec<Token> {
+pub fn lex(s: String) -> Result<Vec<Token>> {
     let mut tokens = Vec::new();
     let mut iter = s.chars().into_iter().peekable();
     while iter.peek().is_some() {
-        let curr = iter.peek();
-        match curr {
-            Some(c) => match c {
-                c if c.is_numeric() => {
-                    tokens.push(lex_number(&mut iter));
+        if let Some(c) =
+            iter.next_if(|v| matches!(v, '+' | '-' | '=' | '{' | '}' | '!' | '>' | '<'))
+        {
+            if let Some(c2) = iter.next_if(|v| matches!(v, '=')) {
+                match (c, c2) {
+                    ('!', '=') => tokens.push(Token::Symbol(SymbolToken::NotEquals)),
+                    ('=', '=') => tokens.push(Token::Symbol(SymbolToken::DoubleEquals)),
+                    ('>', '=') => tokens.push(Token::Symbol(SymbolToken::GreaterEqual)),
+                    ('<', '=') => tokens.push(Token::Symbol(SymbolToken::LessEqual)),
+                    _ => bail!("Invalid Symbol"),
                 }
-                '+' | '-' | '=' => match lex_symbol(c) {
-                    Some(tok) => {
-                        tokens.push(Token::Symbol(tok));
+            } else {
+                dbg!(&c);
+                match lex_symbol(&c) {
+                    Some(tok) => tokens.push(Token::Symbol(tok)),
+                    None => bail!("Uncheck Clause (Symbol Lexing)"),
+                }
+            }
+        } else {
+            if let Some(c) = iter.peek() {
+                match c {
+                    _ if c.is_numeric() => tokens.push(lex_number(&mut iter)),
+                    _ if c.is_alphanumeric() => tokens.push(lex_word(&mut iter)),
+                    ';' => {
+                        iter.next();
+                        tokens.push(Token::Semicolon);
+                    }
+                    _ => {
                         iter.next();
                     }
-                    None => {
-                        iter.next();
-                    }
-                },
-                ';' => {
-                    tokens.push(Token::Semicolon);
-                    iter.next();
                 }
-                _ if c.is_alphabetic() => tokens.push(lex_word(&mut iter)),
-                _ => {
-                    iter.next();
-                }
-            },
-            None => {}
+            }
         }
     }
-    tokens
+    Ok(tokens)
 }
