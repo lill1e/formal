7702ba3c8a76f2b20bf23c05a07eafcd99bd1277
lillie <onlinechronically@protonmail.com>
2025-12-31 06:27:03 -0500

    add: multiple types of results for interpreter

--- src/interp.rs
+++ src/interp.rs
@@ -1,14 +1,41 @@
 use std::collections::HashMap;
 
 use crate::parser::Node;
+use anyhow::{Ok, Result, bail};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Returnable {
+    Number(i32),
+    Boolean(bool),
+    Void,
+}
+
+impl ToString for Returnable {
+    fn to_string(&self) -> String {
+        match self {
+            Returnable::Number(n) => String::from(n.to_string()),
+            Returnable::Boolean(b) => String::from(b.to_string()),
+            Returnable::Void => String::from("Void"),
+        }
+    }
+}
+
+impl Returnable {
+    fn unwrap_int(&self) -> Result<i32> {
+        match self {
+            Returnable::Number(n) => Ok(*n),
+            _ => bail!("Invalid Type"),
+        }
+    }
+}
 
 impl Node {
-    pub fn interpret(&self, env: &mut HashMap<String, i32>) -> i32 {
+    pub fn interpret(&self, env: &mut HashMap<String, Returnable>) -> Returnable {
         match self {
-            Node::Number(n) => *n,
             Node::Addition(n, m) => n.interpret(env) + m.interpret(env),
             Node::Subtraction(n, m) => n.interpret(env) - m.interpret(env),
             Node::Void => Returnable::Void,
+            Node::Number(n) => Returnable::Number(*n),
             Node::Begin(nodes, last) => {
                 for node in nodes {
                     node.interpret(env);
